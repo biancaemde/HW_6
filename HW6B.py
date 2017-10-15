316e34b77ba517403bcea1f8cd78d3e3f7f9f80c
Bianca Emde <biancaemde@gmail.com>
2017-10-15 11:55:34 -0400

    finished part B

--- HW6B.py
+++ HW6B.py
@@ -2,6 +2,7 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
+import re
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
@@ -9,10 +10,14 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
+count = int(input("Enter count: "))
+position = int(input("Enter position: "))
 
-# Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+list = []
+
+for x in range(count):
+	html = urllib.request.urlopen(url, context=ctx).read()
+	soup = BeautifulSoup(html, 'html.parser')
+	url = soup.find_all('a')[position - 1].get('href')
+	list.append(url)
+	print ('Retrieving: ', url)
